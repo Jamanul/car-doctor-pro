53ef9a87ad2842d1e497aca1d3b0c1f6b4a84853
Jamanul <jk.skb.25@gmail.com>
2024-08-04 22:45:00 +0600

    helo

--- src/app/components/Banner.jsx
+++ src/app/components/Banner.jsx
@@ -2,22 +2,57 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-      
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide4" className="btn btn-circle">❮</a>
-            <a href="#slide2" className="btn btn-circle">❯</a>
-          </div>
-        </div>
-      </div>
+       <div className="container mx-auto">
+        <div className="carousel h-screen w-full">
+         {
+            bannerData.map((banner,idx)=>
+            <div style={{
+                backgroundImage: `url('/assets/images/banner/${idx+1}.jpg')`
+            }} id={`slide${idx+1}`} className={`carousel-item relative w-full`}>
+                <div>
+                    <h2 className='text-white text-6xl pt-96'>{banner.heading}</h2>
+                <h2 className='text-white text-3xl pt-6'>{banner.description}</h2>
+                <button className='btn-primary bg-primary p-4 rounded-md ml-4'>Discover More</button>
+                <button className='btn-primary btn-outline border border-primary p-4 rounded-md ml-4'>Latest project</button>
+                </div>
+                
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href={banner.next} className="btn btn-circle">❮</a>
+                <a href={banner.prev} className="btn btn-circle">❯</a>
+              </div>
+            </div>
+          )
+        }
+       </div>
+       </div>
     );
 };
 
 const bannerData=[
     {
-
-    }
+        heading: "Affordable price for car servicing",
+        description: "there are many variation of available services and we are the best",
+        next:"#slide2",
+        prev: "#slide4",
+    },
+    {
+        heading: "Affordable price for car servicing",
+        description: "there are many variation of available services and we are the best",
+        next:"#slide3",
+        prev: "#slide1",
+    },
+    {
+        heading: "Affordable price for car servicing",
+        description: "there are many variation of available services and we are the best",
+        next:"#slide4",
+        prev: "#slide2",
+    },
+    {
+        heading: "Affordable price for car servicing",
+        description: "there are many variation of available services and we are the best",
+        next:"#slide1",
+        prev: "#slide3",
+    },
 ]
 
 
---
+++ src/app/components/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {services} from 
+const Services = () => {
+    console.log(services)
+    return (
+        <div className='mt-4'>
+            <h2 className='text-2xl text-center text-primary'>Services</h2>
+            <h2 className='text-5xl text-center '>Our Service area</h2>
+
+        </div>
+    );
+};
+
+export default Services;
